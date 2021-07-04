d42aa0405c49e343d18b3a6650ef994d2eae7065
mustapha335 <reezy77jay@gmail.com>
2021-07-04 21:25:04 +0100

    change made to the App.js and index.css

--- src/App.js
+++ src/App.js
@@ -5,12 +5,34 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/"
 }
 function App() {
+
+
+  const dateGen = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+    ];
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day= days[d.getDay()];
+    let date = d.getDate();
+    let month=months[d.getMonth()];
+    let year= d.getFullYear();
+    
+    return ` ${day} ${date} ${month} ${year}`;
+  }
   return (
     <div className="app warm">
       <main>
       <div className="search-box">
-      <input type="text" className="search-Bar" placeholder="Search.."></input>
-
+       <input type="text" className="search-bar" placeholder="Search.."></input>
+      </div>
+      <div className="location-box">
+        <div className="location">London</div>
+        <div className="date">{dateGen(new Date())}</div>
+      </div>
+      <div className="weather-box">
+        <div className="temp">20°</div>
+      
+        <div className="weather">Windy</div>
       </div>
 
       </main>
